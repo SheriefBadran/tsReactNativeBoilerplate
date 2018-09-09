9f44f52761b0846e773a316a2544a2557eb8085d
ntwigs <oscar.cv.nordquist@gmail.com>
2018-09-09 18:55:56 +0200

    fix: remove comment (copy of code being used)

--- src/screens/signed-out-screen.tsx
+++ src/screens/signed-out-screen.tsx
@@ -1,23 +1,6 @@
 import * as React from 'react'
 import {Button, View} from 'react-native'
 
-// export const SignedOut = ({navigation}) =>
-//   <View style={{
-//     display: 'flex',
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     height: '100%',
-//     paddingVertical: 20,
-//   }}>
-//     <Button
-//       style={{
-//         backgroundColor: '#03A9F4'
-//       }}
-//       title='SIGN IN'
-//       onPress={() => navigation.navigate('SignedIn')}
-//     />
-//   </View>
-
 export const SignedOut = ({navigation}) => (
   <View style={{
     display: 'flex',
