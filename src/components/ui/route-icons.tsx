4346c260d364728ecb7627fc70d0579529432679
ntwigs <oscar.cv.nordquist@gmail.com>
2018-09-09 19:33:00 +0200

    fix: removed comment

--- src/components/ui/route-icons.tsx
+++ src/components/ui/route-icons.tsx
@@ -1,11 +1,5 @@
 import * as React from 'react'
 import {Icon} from 'react-native-elements'
-// import styled from 'styled-components/native'
-
-// export const TabBarIconImage = styled.Image`
-//   width: 38;
-//   height: 38;
-// `
 
 export const HomeIcon = ({focused}) =>
   focused
