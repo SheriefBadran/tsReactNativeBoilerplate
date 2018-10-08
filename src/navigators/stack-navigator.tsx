96fe8fe3348f92b8bf99345172b1bea39ed61f9b
Oscar  Nordquist <sadinterneatmachine@Oscars-MacBook-Pro.local>
2018-10-08 19:48:39 +0200

    fix: Added a sub navigation to about stack

--- src/navigators/stack-navigator.tsx
+++ src/navigators/stack-navigator.tsx
@@ -1,19 +1,27 @@
-import {createStackNavigator, NavigationScreenProps} from 'react-navigation'
+import * as React from 'react'
+import {
+  createStackNavigator,
+  NavigationScreenProps,
+  HeaderBackButton,
+} from 'react-navigation'
 import {headerTitleStyle, headerStyle} from '../components/ui/layout'
 import {menuLeft} from '../components/ui/menu-left'
 import {Welcome} from '../screens/welcome-screen'
 import {About} from '../screens/about-screen'
-import {backArrow} from '../components/ui/back-arrow'
+import {AboutProfile} from '../screens/about-profile-stack'
 
 const config = (title: string) => ({
   navigationOptions: ({navigation}: NavigationScreenProps) => ({
     title,
     headerLeft: ({title, onPress}) =>
-      title ? backArrow({onPress}) : menuLeft({navigation}),
+      title ? <HeaderBackButton onPress={onPress} /> : menuLeft({navigation}),
     headerStyle,
     headerTitleStyle,
   }),
 })
 
 export const WelcomeStack = createStackNavigator({Welcome}, config('Welcome'))
-export const AboutStack = createStackNavigator({About}, config('About'))
+export const AboutStack = createStackNavigator(
+  {About, AboutProfile},
+  config('About'),
+)
